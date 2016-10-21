87e9db226f29681a223a2e99eec12633168fbfce
sevioncent2 <sevincent2@gmail.com>
2016-10-20 23:11:49 -0500

    Sum totals for home screen

--- app/src/main/java/com/frugal/db/SqlDbDataSource.java
+++ app/src/main/java/com/frugal/db/SqlDbDataSource.java
@@ -6,6 +6,12 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import org.apache.commons.lang3.time.DateUtils;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 /**
  * Created by Waffles on 7/5/2014.
  */
@@ -14,6 +20,11 @@ public class SqlDbDataSource {
 
     SQLiteOpenHelper dbhelper;
     SQLiteDatabase db;
+
+    private int WEEK_IN_MILLI = 604800000;
+
+    private int DAY_IN_MILLI = 86400000;
+
 
     public SqlDbDataSource(Context context) {
         dbhelper = new SqlDbHelper(context);
@@ -40,8 +51,90 @@ public class SqlDbDataSource {
         return db.query(table, columns, where, null, null, null, null);
     }
 
-    public Cursor selectAll (String table)
-    {
-        return db.rawQuery("SELECT * FROM " + table,null);
+    public Cursor selectAll(String table) {
+        return db.rawQuery("SELECT * FROM " + table, null);
+    }
+
+    private Cursor getSumWithDate(String query) {
+        return db.rawQuery(query, null);
+    }
+
+    private StringBuilder getSumStringBuilder(String column, String table) {
+        StringBuilder query = new StringBuilder();
+
+        query.append("SELECT SUM(");
+        query.append(column);
+        query.append(") AS total FROM ");
+        query.append(table);
+        query.append(" WHERE ");
+
+        return query;
+    }
+
+    public Cursor getSumOfDay(String column, String table, Date day) {
+
+        StringBuilder query = getSumStringBuilder(column, table);
+
+        query.append(SqlDbHelper.COL_DATE);
+        query.append(" >= ");
+        query.append(getStartOfDay(day).getTime());
+        query.append(" AND ");
+        query.append(SqlDbHelper.COL_DATE);
+        query.append(" <= ");
+        query.append(getEndOfDay(day).getTime());
+
+        return getSumWithDate(query.toString());
+    }
+
+    /**
+     * Returns the SUM of the provided column. @param firstDayOfWeek will be
+     * used to as the start of the date range + 7 days.
+     *
+     * @param column
+     * @param table
+     * @param firstDayOfWeek
+     * @return
+     */
+    public Cursor getSumOfWeek(String column, String table, Date firstDayOfWeek) {
+
+        StringBuilder query = getSumStringBuilder(column, table);
+
+        query.append(SqlDbHelper.COL_DATE);
+        query.append(" >= ");
+        query.append(getStartOfDay(firstDayOfWeek).getTime());
+        query.append(" AND ");
+        query.append(SqlDbHelper.COL_DATE);
+        query.append(" <= ");
+        query.append(getStartOfDay(firstDayOfWeek).getTime() + WEEK_IN_MILLI);
+
+        return getSumWithDate(query.toString());
+    }
+
+    public Cursor getSumOfMonth(String column, String table, int calendarMonth, int year) {
+
+        Calendar cal = new GregorianCalendar(year, calendarMonth, 1);
+
+        StringBuilder query = getSumStringBuilder(column, table);
+
+        query.append(SqlDbHelper.COL_DATE);
+        query.append(" >= ");
+        query.append(getStartOfDay(cal.getTime()).getTime());
+        query.append(" AND ");
+        query.append(SqlDbHelper.COL_DATE);
+        query.append(" <= ");
+
+        cal = new GregorianCalendar(year,calendarMonth,cal.getActualMaximum(Calendar.DAY_OF_MONTH));
+
+        query.append(getEndOfDay(cal.getTime()).getTime());
+
+        return getSumWithDate(query.toString());
+    }
+
+    public static Date getEndOfDay(Date date) {
+        return DateUtils.addMilliseconds(DateUtils.ceiling(date, Calendar.DATE), -1);
+    }
+
+    public static Date getStartOfDay(Date date) {
+        return DateUtils.truncate(date, Calendar.DATE);
     }
 }
--- app/src/main/java/com/frugal/main/MainActivity.java
+++ app/src/main/java/com/frugal/main/MainActivity.java
@@ -1,13 +1,34 @@
 package com.frugal.main;
 
 import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.TextView;
+
+import com.frugal.db.SqlDbHelper;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import org.apache.commons.io.IOUtils;
+
+import java.text.NumberFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends DrawerActivity {
 
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +40,131 @@ public class MainActivity extends DrawerActivity {
 
         drawerLayout.addView(contentView, 0);
 
+        updateAmounts();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
+    private void updateAmounts() {
+        updateDayAmount();
+        updateWeekAmount();
+        updateMonthAmount();
+    }
+
+    private void updateMonthAmount() {
+        TextView tv1 = (TextView) findViewById(R.id.monthTotal);
+        Cursor cursor = null;
+        try {
+            Calendar cal = Calendar.getInstance();
+
+            cursor = dataSource.getSumOfMonth(SqlDbHelper.COL_AMOUNT, SqlDbHelper.TABLE_EXPENSES, cal.get(Calendar.MONTH), cal.get(Calendar.YEAR));
+
+            NumberFormat formatter = NumberFormat.getCurrencyInstance();
+
+            double totalDouble;
+
+            if (cursor.moveToNext())
+                totalDouble = cursor.getDouble(0);
+            else
+                totalDouble = 0;
+
+            tv1.setText(formatter.format(totalDouble));
+
+        } catch (Exception e) {
+            Log.v("MainActivity", e.getMessage());
+        } finally {
+            IOUtils.closeQuietly(cursor);
+        }
+    }
+
+    private void updateWeekAmount() {
+        TextView tv1 = (TextView) findViewById(R.id.weekTotal);
+        Cursor cursor = null;
+        try {
+            Calendar c = Calendar.getInstance();
+            c.set(Calendar.DAY_OF_WEEK, Calendar.SUNDAY);
+
+            cursor = dataSource.getSumOfWeek(SqlDbHelper.COL_AMOUNT, SqlDbHelper.TABLE_EXPENSES, c.getTime());
+
+
+            NumberFormat formatter = NumberFormat.getCurrencyInstance();
+
+            double totalDouble;
+
+            if (cursor.moveToNext())
+                totalDouble = cursor.getDouble(0);
+            else
+                totalDouble = 0;
+
+            tv1.setText(formatter.format(totalDouble));
+
+        } catch (Exception e) {
+            Log.v("MainActivity", e.getMessage());
+        } finally {
+            IOUtils.closeQuietly(cursor);
+        }
+    }
+
+    private void updateDayAmount() {
+        TextView tv1 = (TextView) findViewById(R.id.dayTotal);
+        Cursor cursor = null;
+        try {
+            cursor = dataSource.getSumOfDay(SqlDbHelper.COL_AMOUNT, SqlDbHelper.TABLE_EXPENSES, new Date());
+
+
+            NumberFormat formatter = NumberFormat.getCurrencyInstance();
+
+            double totalDouble;
+
+            if (cursor.moveToNext())
+                totalDouble = cursor.getDouble(0);
+            else
+                totalDouble = 0;
+
+            tv1.setText(formatter.format(totalDouble));
+
+        } catch (Exception e) {
+            Log.v("MainActivity", e.getMessage());
+        } finally {
+            IOUtils.closeQuietly(cursor);
+        }
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
 }
